4cfb50466da6b62dfda32b30cf67d9517ae1b5e6
Vlada024 <91543583+Vlada024@users.noreply.github.com>
2025-11-17 23:34:43 +0000

    Initial React app structure and DotCounterTrainer component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DotCounterTrainer from './components/DotCounterTrainer';
+import './styles/index.css';
+
+function App() {
+  return <DotCounterTrainer />;
+}
+
+export default App;
